dcb3dd4873ff45f7a3c244cc251dcf92c419c0d7
Mark Vayngrib <mark.vayngrib@lablz.com>
2014-11-10 23:31:14 -0800

    add install script

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir -p bitjoe
+mkdir -p bitjoe/conf
+wget https://github.com/urbien/bitjoe/raw/master/build/bitjoe.jar -P bitjoe
+wget https://github.com/urbien/bitjoe/raw/master/conf/config.json -P bitjoe/conf
